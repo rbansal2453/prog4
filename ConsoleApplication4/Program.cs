29fcae659a10fcfeaa2a57f22f6e803d348bfa9c
Rajesh <Rajesh@RAJESH-PC>
2017-02-20 23:40:26 -0600

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Class1
+    {
+        public int Sum(int A, int B)
+        {
+            return A + B;
+        }
+
+        public float Sum(int A, float B)
+        {
+            return A + B;
+        }
+    }
+
+    class Class2 : Class1
+    {
+        public int Sum(int A, int B, int C)
+        {
+            return A + B + C;
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+
+            Class2 obj = new Class2();
+
+            Console.WriteLine(obj.Sum(10, 20));
+
+            Console.WriteLine(obj.Sum(10, 15.70f));
+
+            Console.WriteLine(obj.Sum(10, 20, 30));
+
+            Console.Read();
+        }
+
+    }
+}
+
